3f35b88f4ae49acb8bf737d9215cf9a31e4d142c
Markus <brandonnarciso24@gmail.com>
2026-09-03 20:47:41 -0700

    forgot to add files

---
+++ server.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+
+@app.get("/ping", response_class=PlainTextResponse)
+def ping() -> str:
+    return "pong"
